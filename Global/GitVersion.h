4a0dc77feab861376703337f5e1f79ee02e306fc
Alexandre Gauthier <alexandre230@msn.com>
2015-07-23 18:32:23 +0200

    conflict

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,10 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
-<<<<<<< HEAD
 #define GIT_BRANCH "libmv"
 #define GIT_COMMIT "c209d6b399c744d6f663c67b0dd9304580a0b7fb"
-=======
-#define GIT_BRANCH "workshop"
-#define GIT_COMMIT "0c4fe448e89c7bac69e5a223f7be004ea92b9773"
->>>>>>> workshop
 #endif
